82fd282d41efb039d3a4787938af8d9d8fb29db8
Azusdex <azusdex@gmail.com>
2015-05-03 11:54:19 +0300

    aaCommit

---
+++ src/Azusdex/NexmoBundle/Controller/NexmoController.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: konstantin.khotski
+ * Date: 5/3/2015
+ * Time: 11:42 AM
+ */
+
+class NexmoController extends \Symfony\Bundle\FrameworkBundle\Controller\Controller{
+
+}
